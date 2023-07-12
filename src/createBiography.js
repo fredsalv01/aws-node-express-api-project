f2a4276c1383dff3c796cbb93787311c9b08f347
Freddy Morales <freddy.morales@usil.pe>
2023-07-12 06:54:30 -0500

    post endpoint complete

---
+++ src/createBiography.js
@@ -0,0 +1,24 @@
+"use strict";
+const AWS = require("aws-sdk");
+const { v4 } = require("uuid");
+
+module.exports.createBiography = async (event) => {
+  const dynamoDb = new AWS.DynamoDB.DocumentClient();
+
+  const { characterId, description } = JSON.parse(event.body);
+  const primary_key = v4();
+  const newBiography = {
+    primary_key,
+    characterId,
+    description,
+  }
+  await dynamoDb.put({
+    TableName: "BiographyTable",
+    Item: newBiography
+  }).promise();
+
+  return {
+    statusCode: 201,
+    body: JSON.stringify(newBiography)
+  };
+};
--- src/swapi.js
+++ src/swapi.js
@@ -34,12 +34,6 @@ app.get("/", async (req, res, next) => {
   }
 });
 
-app.get("/path", (req, res, next) => {
-  return res.status(200).json({
-    message: "Hello from path!",
-  });
-});
-
 app.use((req, res, next) => {
   return res.status(404).json({
     error: "Not Found",
